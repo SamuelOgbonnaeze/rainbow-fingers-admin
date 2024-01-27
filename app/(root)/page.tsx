6c98d471a1080add3ea25f48dd2d2c0a2e1e6d57
SamuelOgbonnaeze <nkwachukwusam01@gmail.com>
2024-01-27 03:10:08 +0100

    Modal Conponent successfully set up

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,24 @@
 "use client"
 
-import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useEffect } from "react";
+
 
 
 const SetupPage = () => {
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen])
+
   return (
     <div>
-      <Modal title="Test" description="Description" isOpen onClose={() => { }}>
-        Children
-      </Modal>
+      root page
+
     </div>
   );
 }
