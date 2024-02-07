b21c53f67d4aa633578ad2ab08ce4c1d209c984d
SamuelOgbonnaeze <nkwachukwusam01@gmail.com>
2024-02-07 10:02:49 +0100

    Navbar Admin setup

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,14 +1,37 @@
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, auth } from "@clerk/nextjs";
 
+import Image from "next/image";
 
-import { MainNav } from "./main-nav";
+import { MainNav } from "@/components/main-nav";
+import StoreSwitcher from "@/components/store-switcher";
+import logo from "@/public/images/logo.png";
+import { redirect } from "next/navigation";
+import prismadb from "@/lib/prismadb";
 
-const Navbar = () => {
+const Navbar = async () => {
+    const { userId } = auth()
+
+    if (!userId) {
+        redirect("/sign-in")
+    }
+
+    const stores= await prismadb.store.findMany({
+        where:{
+            userId
+        }
+    })
+
     return (
         <div className="border-b">
             <div className="h-16 flex items-center px-4 ">
-                <div>
-                    This is the store swithcher
+                <div className="flex items-center space-x-2">
+                    <Image
+                        src={logo}
+                        alt="Rainbow Fingers"
+                        width={80}
+                        height={60}
+                    />
+                    <StoreSwitcher items={stores} />
                 </div>
                 <MainNav className="mx-6" />
                 <div className="ml-auto flex items-center space-x-4">
