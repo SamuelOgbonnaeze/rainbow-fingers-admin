b6e164a26c4be2468d426be6d89d048d2ed89f37
SamuelOgbonnaeze <nkwachukwusam01@gmail.com>
2024-02-16 14:27:36 +0100

    Billboard api routes and cloudinary set up

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
@@ -12,7 +12,6 @@ import { Trash } from "lucide-react";
 import { useOrigin } from "@/hooks/use-origin"
 
 import { AlertModal } from "@/components/modals/alert-modal";
-import { ApiAlert } from "@/components/ui/api-alert";
 import { Heading } from "@/components/ui/heading";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
@@ -25,6 +24,7 @@ import {
     FormMessage
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import ImageUpload from "@/components/ui/image-upload";
 
 const formSchema = z.object({
     label: z.string().min(1),
@@ -62,9 +62,15 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({ initialData }) => 
     const onSubmit = async (data: BillboardFormValues) => {
         try {
             setLoading(true);
-            await axios.patch(`/api/stores/${params.storeId}`, data)
+            if(initialData){
+                await axios.patch(`/api/${params.storeId}/billboards/${params.billboardId}`, data)
+            }else{
+                await axios.post(`/api/${params.storeId}/billboards`, data)
+            }
+           
             router.refresh();
-            toast.success("Store updated")
+            router.push(`/${params.storeId}/billboards`)
+            toast.success(toastMessage)
         } catch (error) {
             toast.error("Something went wrong")
         } finally {
@@ -75,12 +81,12 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({ initialData }) => 
     const onDelete = async () => {
         try {
             setLoading(true)
-            await axios.delete(`/api/stores/${params.storeId}`)
-            toast.success('Store deleted')
+            await axios.delete(`/api/${params.storeId}/billboards/${params.billboardId}`)
+            toast.success('Billboard deleted')
             router.refresh()
             router.push("/")
         } catch (error) {
-            toast.error("Make sure you removed all products and categories first")
+            toast.error("Make sure you removed all categories using this billboard first")
         } finally {
             setOpen(false)
             setLoading(false)
@@ -116,6 +122,24 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({ initialData }) => 
             <Separator />
             <Form {...form} >
                 <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full" >
+                    <FormField
+                        control={form.control}
+                        name="imageUrl"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Background Image</FormLabel>
+                                <FormControl>
+                                    <ImageUpload
+                                        value={field.value ? [field.value] : []}
+                                        disabled={loading}
+                                        onChange={(url) => field.onChange(url)}
+                                        onRemove={() => field.onChange("")}
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
                     <div className="grid grid-cols-3 gap-8">
                         <FormField
                             control={form.control}
--- components/ui/image-upload.tsx
+++ components/ui/image-upload.tsx
@@ -1,6 +1,12 @@
 "use client"
 
+import { CldUploadWidget } from 'next-cloudinary';
+
+import Image from "next/image"
 import { useEffect, useState } from "react";
+import { ImagePlus, Trash } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
 
 interface ImageUploadProps {
     disabled: boolean;
@@ -22,13 +28,51 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
         setIsMounted(true)
     }, []);
 
+    const onUpload = (result: any) => {
+        onChange(result.info.secure_url);
+    }
+
     if (!isMounted) {
         return null
     }
 
+
     return (
         <div>
-            Image Upload
+            <div className="mb-4 flex items-center gap-4">
+                {value.map((url) => (
+                    <div key={url} className="relative w-[200px] h-[200px] rounded-md overflow-hidden " >
+                        <div className="z-10 absolute top-2 right-2">
+                            <Button type="button" onClick={() => onRemove(url)} variant="destructive" size="icon">
+                                <Trash className="w-4 h-4" />
+                            </Button>
+                        </div>
+                        <Image
+                            fill
+                            className="object-cover"
+                            alt="Image"
+                            src={url}
+                        />
+                    </div>
+                ))}
+            </div>
+            <CldUploadWidget onUpload={onUpload} uploadPreset='we248ez0'>
+                {({ open }) => {
+                    const onClick = () => { open() }
+
+                    return (
+                        <Button
+                            type='button'
+                            disabled={disabled}
+                            variant="secondary"
+                            onClick={onClick}
+                        >
+                            <ImagePlus className="h-4 w-4 mr-2" />
+                            Upload an image
+                        </Button>
+                    )
+                }}
+            </CldUploadWidget>
         </div>
     );
 }
