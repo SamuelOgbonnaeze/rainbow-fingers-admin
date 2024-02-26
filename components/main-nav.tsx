c75ec22340dd50783a277c1d6dc454509ae9a872
SamuelOgbonnaeze <nkwachukwusam01@gmail.com>
2024-02-26 19:31:44 +0100

    Product sshema model and section added to dashboard

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -46,6 +46,11 @@ export function MainNav({
             active: pathname === `/${params.storeId}/brands`
         },
         {
+            href: `/${params.storeId}/products`,
+            label: "Products",
+            active: pathname === `/${params.storeId}/products`
+        },
+        {
             href: `/${params.storeId}/settings`,
             label: "Settings",
             active: pathname === `/${params.storeId}/settings`
