b0e9b0774e9d7e21cd5f3eab503dd93570321359
SamuelOgbonnaeze <nkwachukwusam01@gmail.com>
2024-10-09 13:18:21 +0100

    get total revenue modified to fit new schema for order Item

--- actions/get-total-revenue.ts
+++ actions/get-total-revenue.ts
@@ -1,7 +1,5 @@
 import prismadb from "@/lib/prismadb";
 
-
-
 export const getTotalRevenue = async (storeId: string) => {
     const paidOrders = await prismadb.order.findMany({
         where: {
@@ -11,19 +9,24 @@ export const getTotalRevenue = async (storeId: string) => {
         include: {
             orderItems: {
                 include: {
-                    product: true
+                    product: true,
+                    course: true,
                 }
             }
         }
-    })
+    });
 
+   
     const totalRevenue = paidOrders.reduce((total, order) => {
         const orderTotal = order.orderItems.reduce((orderSum, item) => {
-            return orderSum + item.product.price.toNumber();
+            // Use optional chaining to safely access properties
+            const productPrice = item.product?.price?.toNumber() || 0; // Default to 0 if product or price is null
+            const coursePrice = item.course?.price || 0; // Default to 0 if course or price is null
+            return orderSum + productPrice + coursePrice;
         }, 0);
 
         return total + orderTotal;
-    }, 0)
+    }, 0);
 
-    return totalRevenue
-}
+    return totalRevenue;
+};
